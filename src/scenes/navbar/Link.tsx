2f7918ff2728b1b02d80a66fe8e01999e6ee1618
MShahryiar <muhammadshahryiar@yahoo.com>
2023-05-12 09:53:25 +0500

    Link-Index-App Selection/State

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,12 +1,27 @@
 import AnchorLink from "react-anchor-link-smooth-scroll"
 
-type Props = {}
+type Props = {
+  page:string;
+  selectedPage:string;
+  setSelectedPage:(value: string)=> void;
+
+}
 
 function Link({
     page,
+    selectedPage,
+    setSelectedPage,
 }: Props) {
+  const lowerCasePage = page.toLowerCase().replace(/ /g,"")
+
   return (
-    <AnchorLink>
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-primary-500":""}
+            transition duration-500 hover:text-primary-500
+      `}
+      href={`#${lowerCasePage}`}
+      onClick={()=> setSelectedPage(lowerCasePage)}
+    >
         {page}
     </AnchorLink>
   )
