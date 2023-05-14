ab3bf2f6a5024ddf60c96973a98aaee365857d0b
MShahryiar <muhammadshahryiar@yahoo.com>
2023-05-14 17:21:32 +0500

    Responsiveness

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,6 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
 import Logo from "@/assets/Logo.png"
 import Link from "./Link"
 import { SelectedPage } from "@/shared/types"
+import useMediaQuery from "@/hooks/useMediaQuery"
 
 
 
@@ -13,7 +14,8 @@ type Props = {
 
 const Navbar = ({selectedPage, setSelectedPage}: Props) => {
   const flexBetween = "flex items-center justify-between"
-
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false)
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)")
   return <nav>
        <div className={`${flexBetween}  fixed top-0 z-30 w-full py-6`}>
             <div className={`${flexBetween}  mx-auto w-5/6`}>
@@ -21,24 +23,26 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
                     {/* LEFT  */} 
                     <img alt="Logo" src={Logo}/>
                     {/* Right */}
-                    <div className={`${flexBetween}  w-full`}>
+                    {isAboveMediumScreens ? 
+                    (
+                        <div className={`${flexBetween}  w-full md:flex hidden `}>
                         <div className={`${flexBetween} gap-8  text-sm`}>
                             <Link 
                                 page="Home"
                                 selectedPage={selectedPage}
                                 setSelectedPage={setSelectedPage}
                                 
-                            />
+                                />
                             <Link 
                                 page="Benefits"
                                 selectedPage={selectedPage}
                                 setSelectedPage={setSelectedPage}
-                            />
+                                />
                             <Link 
                                 page="Our Classes"
                                 selectedPage={selectedPage}
                                 setSelectedPage={setSelectedPage}
-                            />
+                                />
                             <Link 
                                 page="Contact Us"
                                 selectedPage={selectedPage}
@@ -51,6 +55,15 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
                             <button>Become a member</button>
                         </div>
                     </div>
+                    )                                
+                    :
+                     (
+                                            
+
+                        <button className=" bg-secondary-500 rounded-full flex md:hidden" onClick={()=>setIsMenuToggled(!isMenuToggled)}>
+                            <Bars3Icon className="h-6  text-white  p-1 w-6"/>
+                        </button>                    
+                    )}
 
                </div>
                
@@ -60,3 +73,6 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
 }
 
 export default Navbar
+
+{/* <button className=" bg-secondary-500 rounded-full flex md:hidden" onClick={()=>setIsMenuToggled(!isMenuToggled)}> */}
+{/* <div className={`${flexBetween}  w-full md:flex hidden `}></div> */}
