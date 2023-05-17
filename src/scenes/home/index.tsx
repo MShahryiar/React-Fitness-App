f38b2e0cdef79dad4f2182a16b7526a687b5a0d7
MShahryiar <muhammadshahryiar@yahoo.com>
2023-05-17 11:48:57 +0500

    Home Page 2/2

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -15,24 +15,25 @@ type Props = {
 
 function Home({setSelectedPage}: Props) {
   const isAboveMediaScreens = useMediaQuery("(min-width:1060px)")
-  return  <section id="home" className='gap-16 bg-gray-20 md:h-full md:pb-0'>
+  return  <section id="home" 
+  className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
         {/* image and main header */}
-        <div>
+        <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
             {/* Main header */}
-            <div>
+            <div className='z-10 mt-32  md:basis-3/5'>
                 {/* Headings */}
-                <div>
-                    <div>
-                        <div>
+                <div className='md:-mt-20'>
+                    <div className='relative'>
+                        <div className='before:absolute before:-top-20 before:z-[-1] before:-left-20  md:before:content-evolvetext'>
                              <img alt="Home-page-text" src={HomePageText}/>
                         </div>
                     </div>
-                    <p>Unrived Gym. Unparalleled Training Fitness Classes. World Class
+                    <p className='mt-8 text-sm'> Unrived Gym. Unparalleled Training Fitness Classes. World Class
                         Studios to get the Body Shape that you dream of... Get your Dream body now.
                     </p>
                 </div>
                 {/* Actions buttons */}
-                <div>
+                <div className='mt-8 flex items-center gap-8'>
                     <ActionButton setSelectedPage={setSelectedPage}>
                         Join Now
                     </ActionButton>
@@ -46,18 +47,19 @@ function Home({setSelectedPage}: Props) {
                 </div>
             </div>
         {/* image */}
-        <div>
+        <div className='flex basic-3/5 justify-center md:z-10
+        md:ml-40 md:mt-16 md:justify- items-end'>
             <img src={HomePageGraphic} alt="Home-page-graphic"/>
         </div>
         </div>
         {/* Sponsors */}
         {isAboveMediaScreens && (
-            <div>
-                <div>
-                    <div>
-                        <img alt="Redbull-sponsor" src={SponsorRedBull} />
-                        <img alt="Forbes-sponsor" src={SponsorForbes} />
-                        <img alt="Fortune-sponsor" src={SponsorFortune} />
+            <div className='h-[150px] w-full bg-primary-100'>
+                <div className="w-5/6 mx-auto ">
+                    <div className='flex gap-8 items-center justify-between w-3/5'>
+                        <img alt="Redbull-sponsor" className='mt-10' src={SponsorRedBull} />
+                        <img alt="Forbes-sponsor" className='mt-10' src={SponsorForbes} />
+                        <img alt="Fortune-sponsor"  className='mt-10' src={SponsorFortune} />
 
                     </div>
                 </div>
